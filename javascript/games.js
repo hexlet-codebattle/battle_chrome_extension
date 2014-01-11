5724665d5ec0d1d10c944e5fe4e2396808635dbe
plugin73 <laplugin73@gmail.com>
2014-01-11 21:42:35 +0100

    show only opened games

--- javascript/games.js
+++ javascript/games.js
@@ -1,24 +1,35 @@
 var games = []
+
 $(function(){
-  function establish_connection(){
+  (function establish_connection(){
     var bullet = $.bullet('ws://wddx.ru/ws/games');
     bullet.onopen = function(){
-        console.log('bullet: opened');
+      console.log('bullet: opened');
+      chrome.browserAction.setBadgeText({text: "0"});
     };
+
     bullet.ondisconnect = function(){
-        console.log('bullet: disconnected');
+      console.log('bullet: disconnected');
+      chrome.browserAction.setBadgeText({text: "-"});
     };
+
     bullet.onclose = function(){
-        console.log('bullet: closed');
+      console.log('bullet: closed');
+      chrome.browserAction.setBadgeText({text: "-"});
     };
+
     bullet.onmessage = function(e){
-      games = games.concat(e.data);
+      game = $.parseJSON(e.data)
+      if (game.handler != "openGame"){
+        games = _.reject(games, {data: {id: game.data.id}})
+      }else{
+        games = games.concat(game);
+      }
       chrome.browserAction.setBadgeText({text: games.length + ""});
-      console.log(games);
     };
+
     bullet.onheartbeat = function(){
-        bullet.send('ping');
+      bullet.send('ping');
     }
-  }
-  establish_connection.call();
+  })();
 });
