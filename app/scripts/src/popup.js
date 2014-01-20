19847d6972ec65e35caad6fd75706d88c76e0152
plugin73 <laplugin73@gmail.com>
2014-01-20 20:41:39 +0100

    no open games label

--- app/scripts/src/popup.js
+++ app/scripts/src/popup.js
@@ -17,15 +17,24 @@ $(function() {
     },
 
     render: function() {
-      return <div>
-        <pre>
-          {this.state.messages.map(function (message) {
-            return <BattleGame game={message.data} />;
-          })}
-        </pre>
-      </div>
-    }
-  });
+      //NOTE Мне это не нравится, но JSX не поддерживает if-else
+      if (this.state.messages.length == 0) {
+        return <div>
+          <pre>
+            <label>No open games</label>
+          </pre>
+        </div>
+      }else {
+        return <div>
+          <pre>
+            {this.state.messages.map(function (message) {
+              return <BattleGame game={message.data} />;
+            })}
+          </pre>
+        </div>
+      }
+  }
+});
 
   React.renderComponent(<BattleApp />, document.getElementById('games'));
 });
