4f33791aa536ef2f074b9bdf6dc95e6ccce40a1f
plugin73 <laplugin73@gmail.com>
2014-01-30 21:33:42 +0100

    views for games

--- app/scripts/src/OpenedGames.js
+++ app/scripts/src/OpenedGames.js
@@ -6,16 +6,22 @@ var OpenedGames = React.createClass({
 
   mixins: [SetIntervalMixin],
 
-//[TODO]Написать вьюху конкретно под открытые игры
 //[TODO]Обрабатывать случай, когда нет игр
   render: function() {
     return <div>
       <pre>{
         this.state.messages.map(function (message) {
           href = settings.host + "/games/" + message.data.id + "/join";
-          link = <a href={href} onClick={this.handleLinkClick}>Join</a>;
-          span = <span> to {message.data.id} </span>;
-          return <div>{link} {span}</div>
+
+          link = <a href={href} onClick={this.handleLinkClick}>
+                   {message.data.game.level}{'\u00A0'}
+                   {message.data.player.lang}
+                   game</a>;
+          player = <span>{message.data.player.nickname}</span>;
+
+          game_id = <span>({message.data.id})</span>;
+
+          return <div>{link}: {player}{game_id}</div>
         }, this)
       }</pre>
     </div>
--- app/scripts/src/StartedGames.js
+++ app/scripts/src/StartedGames.js
@@ -6,16 +6,23 @@ var StartedGames = React.createClass({
 
   mixins: [SetIntervalMixin],
 
-//[TODO]Написать вьюху конкретно под начатые игры
 //[TODO]Обрабатывать случай, когда нет игр
   render: function() {
     return <div>
       <pre>{
         this.state.messages.map(function (message) {
+          //[FIXME] получать ссылку на игру с сервера
           href = settings.host + "/games/" + message.data.id + "/join";
-          link = <a href={href} onClick={this.handleLinkClick}>Join</a>;
-          span = <span> to {message.data.id} </span>;
-          return <div>{link} {span}</div>
+
+          link = <a href={href} onClick={this.handleLinkClick}>watch game</a>;
+
+          players = <span>
+                      {_.first(message.data.players).nickname} vs
+                      {_.last(message.data.players).nickname}
+                    </span>;
+          game_id = <span>({message.data.id})</span>;
+
+          return <div>{link}: {players} {game_id}</div>
         }, this)
       }</pre>
     </div>
