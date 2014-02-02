9694087aec89ac3ffc630d4cf06e59d1f5439125
plugin73 <laplugin73@gmail.com>
2014-02-02 19:38:21 +0100

    no games opened

--- app/scripts/src/OpenedGames.js
+++ app/scripts/src/OpenedGames.js
@@ -6,28 +6,29 @@ var OpenedGames = React.createClass({
 
   mixins: [SetIntervalMixin],
 
-//[TODO]Обрабатывать случай, когда нет игр
+  getGames: function() {
+    return this.state.messages.map(function (message) {
+      href = settings.host + "/games/" + message.data.id + "/join";
+
+      game = <span>
+        {message.data.game.level}{'\u00A0'}
+        {message.data.player.lang}
+      </span>;
+
+      return (
+        <div>
+          {game} game by <b>{message.data.player.nickname}</b>.<br/>
+          <LangsLinks
+            langs={message.data.extended_langs}
+            href={href}
+            onLinkClick={this.handleLinkClick}
+          />
+        </div>);
+    }, this);
+  },
+
   render: function() {
-    return <div> {
-        this.state.messages.map(function (message) {
-          href = settings.host + "/games/" + message.data.id + "/join";
-
-          game = <span>
-                   {message.data.game.level}{'\u00A0'}
-                   {message.data.player.lang}
-                 </span>;
-
-          return (
-            <div>
-              {game} game by <b>{message.data.player.nickname}</b>.<br/>
-              <LangsLinks
-                langs={message.data.extended_langs}
-                href={href}
-                onLinkClick={this.handleLinkClick}
-              />
-            </div>);
-        }, this)
-    }</div>
+    return <div>{this.state.messages.length > 0 ? this.getGames() : "No opened games"}</div>
   }
 });
 
--- app/scripts/src/StartedGames.js
+++ app/scripts/src/StartedGames.js
@@ -6,27 +6,29 @@ var StartedGames = React.createClass({
 
   mixins: [SetIntervalMixin],
 
+  getGames: function(){
+    return this.state.messages.map(function (message) {
+      //[FIXME] получать ссылку на игру с сервера
+      href = settings.host + "/games/" + message.data.id;
+      first_player = _.first(message.data.players);
+      second_player = _.last(message.data.players);
+
+      players = <a href={href} onClick={this.handleLinkClick} className="game_link">
+        {first_player.nickname} vs
+        {second_player.nickname}
+      </a>;
+
+      langs = <span>
+        {first_player.lang}/{second_player.lang}
+      </span>
+
+      return <div>{langs} : {players}</div>
+    }, this);
+  },
+
 //[TODO]Обрабатывать случай, когда нет игр
   render: function() {
-    return <div>
-        {this.state.messages.map(function (message) {
-          //[FIXME] получать ссылку на игру с сервера
-          href = settings.host + "/games/" + message.data.id;
-          first_player = _.first(message.data.players);
-          second_player = _.last(message.data.players);
-
-          players = <a href={href} onClick={this.handleLinkClick} className="game_link">
-                      {first_player.nickname} vs
-                      {second_player.nickname}
-                    </a>;
-
-          langs = <span>
-                    {first_player.lang}/{second_player.lang}
-                  </span>
-
-          return <div>{langs} : {players}</div>
-        }, this)}
-    </div>
+    return <div>{this.state.messages > 0 ? this.getGames() : "No started games"}</div>
   }
 });
 
