0b06ae1bb3ac204064e330012158f0ca45a461f0
Ilya Shakirov <aelaau@gmail.com>
2015-09-28 15:13:56 +0300

    Putting app in boilerplate

--- app/components/OpenedGame.js
+++ app/components/OpenedGame.js
@@ -1,29 +1,28 @@
-import React from "react";
+import cx from 'classnames';
 
-export default  React.createClass({
-  render: function() {
-    var message = this.props.message;
-    href = settings.host + "/games/" + message.id + "/join";
+const OpenedGame = (props) => {
+  const message = props.message;
+  let href = 'http://wddx.ru/games/' + message.id + '/join';
 
-    var cx = React.addons.classSet;
-    return (
-      <div>
-        {message.game.level + " " + message.player.lang + " game by "} <a href="#">{message.player.nickname}</a>.
+  return (
+    <div>
+      {message.game.level + ' ' + message.player.lang + ' game by '} <a href='#'>{message.player.nickname}</a>.
 
-        <p>
-          join as:
-            {message.extended_langs.map(function(lang_info) {
-              href = href + "?lang=" + lang_info.lang;
-              var classes = cx({game_passed_link: lang_info.passed});
+      <p>
+        join as:
+        {message.extended_langs.map((langInfo) => {
+          href = href + '?lang=' + langInfo.lang;
+          const classes = cx({game_passed_link: langInfo.passed});
 
-              return (
-                <span className="join-lang">
-                  <a href={href} target="_blank" className={classes}>{lang_info.lang}</a>
-                </span>
-              )
-            }, this)}
-        </p>
-      </div>
-    )
-  }
-});
+          return (
+            <span className='join-lang'>
+              <a href={href} target='_blank' className={classes}>{langInfo.lang}</a>
+            </span>
+            );
+        }, this)}
+      </p>
+    </div>
+  );
+};
+
+export default OpenedGame;
