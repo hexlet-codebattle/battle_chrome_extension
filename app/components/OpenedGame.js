ff2e7bf4e4ba4c074ceee0dad6a4f0ad7045735c
Kirill Mokevnin <mokevnin@gmail.com>
2015-10-08 16:33:50 -0400

    refactoring and remove set interval

--- app/components/OpenedGame.js
+++ app/components/OpenedGame.js
@@ -4,26 +4,14 @@ import SETTINGS from "../../chrome/app/settings";
 
 const OpenedGame = (props) => {
   const game = props.game;
-  const mainHref = SETTINGS.host + "/games/" + game.game.id + "/join";
+  const mainHref = SETTINGS.host + "/games/" + game.game.id;
   const member = game.members[0];
 
   return (
     <div>
-      {game.game.challenge_build.level + " " + member.lang + " game by "} <a href="#">{member.username}</a>.
-
-      <p>
-        join as:
-        {game.game.build_langs.map((langInfo) => {
-          const href = mainHref + "?lang=" + langInfo.lang;
-          const classes = cx({"game-passed-link": langInfo.passed});
-
-          return (
-            <span className="join-lang" key={langInfo.lang}>
-              <a href={href} target="_blank" className={classes}>{langInfo.lang}</a>
-            </span>
-            );
-        })}
-      </p>
+      <a href={mainHref}>
+        {game.game.challenge_build.level + " game by " + member.username}
+      </a>
     </div>
   );
 };
