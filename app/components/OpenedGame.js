5763f136eb66abcd3a5c316a04f1954e78ce30c7
Kirill Mokevnin <mokevnin@gmail.com>
2015-11-15 14:11:04 +0300

    fix app

--- app/components/OpenedGame.js
+++ app/components/OpenedGame.js
@@ -3,14 +3,14 @@ import React, {Component} from "react";
 import SETTINGS from "../../chrome/app/settings";
 
 const OpenedGame = (props) => {
-  const game = props.game;
-  const mainHref = SETTINGS.host + "/games/" + game.game.id;
-  const member = game.members[0];
+  const data = props.game;
+  const mainHref = SETTINGS.host + "/games/" + data.game.id;
+  const member = data.members[0];
 
   return (
     <div>
       <a href={mainHref} target="_blank">
-        {game.game.level + " game by " + member.username}
+        {member.lang + " game by " + member.username}
       </a>
     </div>
   );
